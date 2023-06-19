50dd550f435e3d8812a61c2eeefbab0e45e2080b
Julian Fernandez <julianfernandez1811@gmail.com>
2023-06-19 12:22:03 -0300

    [ADD] clase 14

---
+++ clase_14/Entidades/Persona.cs
@@ -0,0 +1,25 @@
+﻿namespace Entidades
+{
+    public class Persona
+    {
+        // Los valores que reciba estos dos son el valor del objeto
+        private string nombre;
+        private string apellido;
+        //private List<int> numeros;
+
+        public string Nombre { get => this.nombre; set => this.nombre = value;  }
+        public string Apellido { get => this.apellido; set => this.apellido = value; }
+        //public List<int> Numeros { get => numeros; set => numeros = value; }
+
+        public Persona()
+        {
+            //this.Numeros = new List<int>();
+        }
+        public Persona(string nombre, string apellido)
+        {
+            this.nombre = nombre;
+            this.apellido = apellido;
+        }
+
+    }
+}
---
+++ clase_14/clase_14/Program.cs
@@ -0,0 +1,97 @@
+﻿using Entidades;
+using System.Text.Json;
+using System.Text.Json.Nodes;
+using System.Xml.Serialization;
+
+namespace clase_14
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*// Absolute path
+            // El arroba está porque la contrabarra sino rompe porque no lo reconoce como una secuencia válida
+            //File.Create(@"D:\Users\julian\Documents\Julian\UTN\2. Segundo cuatrimestre\labo-progra_2\clase_14\miArchivo.txt");
+
+            // Relative path
+            string ruta = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            char separador = Path.DirectorySeparatorChar;
+            string nombreCarpeta = "Archivos clase 14";
+            string nombreArchivo = "miArchivo.txt";
+            //ruta = $"{ruta}{separador}{nombreCarpeta}{separador}{nombreArchivo}"; // manual
+            ruta = Path.Join(ruta, nombreCarpeta, nombreArchivo); // con Join();
+
+
+
+
+            // Como en python. Para no abrir y cerrar el archivo, el uso de "using" ya abre y cierra el archivo
+            // automáticamente en este bloque de código
+            // Esto puede pisar TODO el archivo si es que el archivo ya tenía info dentro
+            // El parametro booleano es si se va a appendear el texto (sin borrar la info que ya tenga), o no (lo sobreescribe entero)
+            using (StreamWriter sw = new StreamWriter(ruta, true))
+            {
+                sw.WriteLine("Hola mundo");
+            }
+
+            using (StreamReader sr = new StreamReader(ruta, true))
+            {
+                //string lecturaPorLinea = sr.ReadLine();
+                string lectura = sr.ReadToEnd();
+                Console.WriteLine(lectura);
+            }*/
+
+
+            string ruta = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            char separador = Path.DirectorySeparatorChar;
+            string nombreCarpeta = "Archivos clase 14";
+            string nombreArchivo = "miObjeto.xml"; // para serializar en XML
+            string rutaCompleta = Path.Join(ruta, nombreCarpeta, nombreArchivo); // con Join();
+
+
+            Persona persona = new Persona("Julian", "Fernandez");
+            // Serializar en XML
+            using (StreamWriter sw = new StreamWriter(rutaCompleta))
+            {
+                // Le indicamos a qué tipo queremos serializar porque necesita las propiedades/atributos
+                // publicas del objeto
+                XmlSerializer xml = new XmlSerializer(typeof(Persona));
+                xml.Serialize(sw, persona);
+            }
+
+            using (StreamReader sr = new StreamReader(rutaCompleta))
+            {
+                XmlSerializer xml = new XmlSerializer(typeof(Persona));
+                Persona persona2 = (Persona)xml.Deserialize(sr);
+
+                Console.WriteLine(persona2.Nombre);
+                Console.WriteLine(persona2.Apellido);
+            }
+
+
+            // Serializar en JSON
+            nombreArchivo = "miObjeto.json";
+            rutaCompleta = Path.Join(ruta, nombreCarpeta, nombreArchivo);
+
+            using (StreamWriter sw = new StreamWriter(rutaCompleta))
+            {
+                JsonSerializerOptions opciones = new JsonSerializerOptions();
+                opciones.WriteIndented = true;
+                // No se puede instanciar, es clase estática
+                //JsonSerializer json = new JsonSerializer(typeof(Persona));
+                string json = JsonSerializer.Serialize<Persona>(persona, opciones);
+                Console.WriteLine(json);
+                sw.WriteLine(json);
+
+            }
+
+            using (StreamReader sr = new StreamReader(rutaCompleta))
+            {
+                string json = sr.ReadToEnd();
+                Persona persona3 = JsonSerializer.Deserialize<Persona>(json);
+                Console.WriteLine("JSON");
+                Console.WriteLine(persona3.Nombre);
+                Console.WriteLine(persona3.Apellido);
+            }
+        }
+    }
+}
