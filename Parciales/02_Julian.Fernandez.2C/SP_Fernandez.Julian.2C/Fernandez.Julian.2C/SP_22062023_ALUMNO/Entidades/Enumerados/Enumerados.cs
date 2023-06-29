73641364f5c0f21ce79a61532a7cd37642b28805
Julian Fernandez <julianfernandez1811@gmail.com>
2023-06-29 15:31:18 -0300

    [UPD] 2do parcial

--- Parciales/02_Julian.Fernandez.2C/SP_Fernandez.Julian.2C/Fernandez.Julian.2C/SP_22062023_ALUMNO/Entidades/Enumerados/Enumerados.cs
+++
@@ -1,4 +0,0 @@
-﻿namespace Entidades.Enumerados
-{
-    public enum EIngrediente { ADHERESO =5, QUESO = 10, JAMON = 12, HUEVO = 13, PANCETA = 15 };
-}
--- Parciales/02_Julian.Fernandez.2C/SP_Fernandez.Julian.2C/Fernandez.Julian.2C/SP_22062023_ALUMNO/UnitTestingCocina/Usings.cs
+++
@@ -1,3 +0,0 @@
-global using Microsoft.VisualStudio.TestTools.UnitTesting;
-global using Entidades.Excepciones;
-global using Entidades.Files;
