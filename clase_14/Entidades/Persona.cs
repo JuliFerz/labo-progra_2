fa57fdcd73c2d3818cc525cbf5e6cbf3060825e4
Julian Fernandez <julianfernandez1811@gmail.com>
2023-06-29 13:38:17 -0300

    [UPD] clase 14

--- clase_14/Entidades/Persona.cs
+++ clase_14/Entidades/Persona.cs
@@ -2,23 +2,26 @@
 {
     public class Persona
     {
-        // Los valores que reciba estos dos son el valor del objeto
+        // Los valores que reciba estos dos son el valor/estado del objeto
         private string nombre;
         private string apellido;
+        private int edad;
         //private List<int> numeros;
 
+        // Son requeridos las Propiedades para poder extraer el valor de los atributos del objeto.
+        // Sino no es posible obtener el valor de dicho atributo.
         public string Nombre { get => this.nombre; set => this.nombre = value;  }
         public string Apellido { get => this.apellido; set => this.apellido = value; }
-        //public List<int> Numeros { get => numeros; set => numeros = value; }
+        public int Edad { get => this.edad; set => this.edad = value; }
 
         public Persona()
         {
-            //this.Numeros = new List<int>();
         }
-        public Persona(string nombre, string apellido)
+        public Persona(string nombre, string apellido, int edad)
         {
             this.nombre = nombre;
             this.apellido = apellido;
+            this.edad = edad;
         }
 
     }
--- clase_14/clase_14/Program.cs
+++ clase_14/clase_14/Program.cs
@@ -1,4 +1,5 @@
 ﻿using Entidades;
+using System.Reflection.Emit;
 using System.Text.Json;
 using System.Text.Json.Nodes;
 using System.Xml.Serialization;
@@ -14,15 +15,22 @@ namespace clase_14
             //File.Create(@"D:\Users\julian\Documents\Julian\UTN\2. Segundo cuatrimestre\labo-progra_2\clase_14\miArchivo.txt");
 
             // Relative path
-            string ruta = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
-            char separador = Path.DirectorySeparatorChar;
-            string nombreCarpeta = "Archivos clase 14";
-            string nombreArchivo = "miArchivo.txt";
+            string ruta = Environment.GetFolderPath(Environment.SpecialFolder.Desktop); // string con la ruta hasta escritorio
+            //char separador = Path.DirectorySeparatorChar; // caracter separador del path
+            string nombreCarpeta = "carpeta_test";
+            string nombreArchivo = "test.txt";
             //ruta = $"{ruta}{separador}{nombreCarpeta}{separador}{nombreArchivo}"; // manual
-            ruta = Path.Join(ruta, nombreCarpeta, nombreArchivo); // con Join();
+            //ruta = Path.Join(ruta, nombreCarpeta, nombreArchivo); // con Join(); (no hace falta el separador)
+            *//* 
+             * Lo dejo comentado porque lo uso más abajo para consultar si la ruta existe 
+             * (para crear SOLO con el directorio), porque sino se crea el test.txt como si fuera un directorio 
+             *//*
 
 
-
+            ruta = Path.Join(ruta, nombreCarpeta);
+            if (!Directory.Exists(ruta))
+                Directory.CreateDirectory(ruta);
+            ruta = Path.Join(ruta, nombreArchivo);
 
             // Como en python. Para no abrir y cerrar el archivo, el uso de "using" ya abre y cierra el archivo
             // automáticamente en este bloque de código
@@ -40,58 +48,64 @@ namespace clase_14
                 Console.WriteLine(lectura);
             }*/
 
-
             string ruta = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
-            char separador = Path.DirectorySeparatorChar;
-            string nombreCarpeta = "Archivos clase 14";
+            string nombreCarpeta = "carpeta_test";
             string nombreArchivo = "miObjeto.xml"; // para serializar en XML
             string rutaCompleta = Path.Join(ruta, nombreCarpeta, nombreArchivo); // con Join();
 
 
-            Persona persona = new Persona("Julian", "Fernandez");
-            // Serializar en XML
-            using (StreamWriter sw = new StreamWriter(rutaCompleta))
+            Persona persona = new Persona("Julian", "Fernandez", 23);
+            // ******************** Serializar en XML ******************** //
+            /*using (StreamWriter sw = new StreamWriter(rutaCompleta))
             {
-                // Le indicamos a qué tipo queremos serializar porque necesita las propiedades/atributos
+                // Le indicamos qué tipo de objeto queremos serializar porque necesita las propiedades/atributos
                 // publicas del objeto
                 XmlSerializer xml = new XmlSerializer(typeof(Persona));
-                xml.Serialize(sw, persona);
-            }
+                xml.Serialize(sw, persona); // serializa el objeto en formato XML
+            }*/
 
-            using (StreamReader sr = new StreamReader(rutaCompleta))
+            // ******************** Deserializar XML ******************** //
+            /*using (StreamReader sr = new StreamReader(rutaCompleta))
             {
-                XmlSerializer xml = new XmlSerializer(typeof(Persona));
+                XmlSerializer? xml = new XmlSerializer(typeof(Persona));
                 Persona persona2 = (Persona)xml.Deserialize(sr);
 
-                Console.WriteLine(persona2.Nombre);
-                Console.WriteLine(persona2.Apellido);
-            }
+                Console.WriteLine($"{persona2.Apellido}, {persona2.Nombre}. {persona2.Edad} años");
+            }*/
 
 
-            // Serializar en JSON
+            // ******************** Serializar en JSON ******************** //
             nombreArchivo = "miObjeto.json";
             rutaCompleta = Path.Join(ruta, nombreCarpeta, nombreArchivo);
 
-            using (StreamWriter sw = new StreamWriter(rutaCompleta))
+            /*using (StreamWriter sw = new StreamWriter(rutaCompleta))
             {
                 JsonSerializerOptions opciones = new JsonSerializerOptions();
-                opciones.WriteIndented = true;
+                opciones.WriteIndented = true; // establecer que se indente
                 // No se puede instanciar, es clase estática
                 //JsonSerializer json = new JsonSerializer(typeof(Persona));
                 string json = JsonSerializer.Serialize<Persona>(persona, opciones);
                 Console.WriteLine(json);
                 sw.WriteLine(json);
 
-            }
+            }*/
 
-            using (StreamReader sr = new StreamReader(rutaCompleta))
+            // ******************** Deserializar en JSON ******************** //
+            /*using (StreamReader sr = new StreamReader(rutaCompleta))
             {
                 string json = sr.ReadToEnd();
                 Persona persona3 = JsonSerializer.Deserialize<Persona>(json);
-                Console.WriteLine("JSON");
-                Console.WriteLine(persona3.Nombre);
-                Console.WriteLine(persona3.Apellido);
-            }
+                Console.WriteLine($"{persona3.Apellido}, {persona3.Nombre}. {persona3.Edad} años");
+            }*/
+
+            // ******************** Serializar en TXT (archivo plano) ******************** //
+            //nombreArchivo = "miObjeto.txt";
+            //rutaCompleta = Path.Join(ruta, nombreCarpeta, nombreArchivo);
+
+            //using (StreamWriter sw = new StreamWriter(rutaCompleta))
+            //{
+            //    sw.WriteLine(JsonSerializer.Serialize(persona));
+            //}
         }
     }
 }
